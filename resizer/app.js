c3ee1f1a1cd72a350ca98483e9cfa6fb25e969e0
Michael Kachi <jesmikky@gmail.com>
2022-10-27 16:21:37 +0100

    initial commit

---
+++ resizer/app.js
@@ -0,0 +1,64 @@
+const uploadBox = document.querySelector('.upload-box');
+previewImg = uploadBox.querySelector("img");
+fileInput = uploadBox.querySelector("input");
+widthInput = document.querySelector(".width input");
+heightInput = document.querySelector(".height input");
+ratioInput = document.querySelector(".ratio input");
+downloadBtn = document.querySelector(".download-btn");
+qualityInput = document.querySelector(".quality input");
+
+let ogImageRatio;
+
+
+//uploadBox.addEventListener('click', function(){return fileInput.click()})
+
+
+const loadFile = (e) => {
+    const file = e.target.files[0];
+    if(!file) return;
+    previewImg.src = URL.createObjectURL(file)
+    previewImg.addEventListener("load", () => {
+        widthInput.value = previewImg.naturalWidth;
+        heightInput.value = previewImg.naturalHeight;
+        ogImageRatio = previewImg.naturalWidth / previewImg.naturalHeight
+        document.querySelector('.wrapper').classList.add('active')
+    });
+   console.log(file);
+}
+
+widthInput.addEventListener("keyup", () => {
+    const height = ratioInput.checked? widthInput.value/ogImageRatio : heightInput.value;
+    heightInput.value = Math.floor(height);
+})
+
+heightInput.addEventListener("keyup", () => {
+    const width = ratioInput.checked? heightInput.value/ogImageRatio : widthInput.value;
+    widthInput.value = Math.floor(width);
+})
+
+const resizeAndDownload = () => {
+
+   const imageQuality = qualityInput.checked? 0.7 : 1.0;
+    
+    const canvas = document.createElement("canvas");
+    const a = document.createElement("a");
+    const ctx = canvas.getContext("2d");
+    
+
+    canvas.width = widthInput.value;
+    canvas.height = heightInput.value;
+
+    ctx.drawImage(previewImg, 0,0, canvas.width, canvas.height)
+    a.href = canvas.toDataURL("image/jpeg", imageQuality);
+    a.download = new Date().getTime();
+    a.click();
+   // document.body.appendChild(canvas);
+
+
+
+}
+downloadBtn.addEventListener("click", resizeAndDownload);
+fileInput.addEventListener("change", loadFile);
+ uploadBox.addEventListener('click', () => fileInput.click());
+
+
